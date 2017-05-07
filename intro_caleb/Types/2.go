245aa3dbc228e85f5cb40851994029c9f554c8d9
Paulus Donny Junianto <katalisstone@gmail.com>
2017-05-07 15:57:36 +0700

    https://www.golang-book.com/books/intro/3

---
+++ intro_caleb/Types/2.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("Largest 8 digit number :", math.Pow10(8)-1)
+}
---
+++ intro_caleb/Types/4.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("String is individual bytes. (Using len)")
+	fmt.Println("This Length is : ", len("String is individual bytes."))
+}
